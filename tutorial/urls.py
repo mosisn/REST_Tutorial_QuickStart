b15a23b91cf09738d000cc1af08a8fe2a0cf9831
Mostafa <mostafa.saneisn@gmail.com>
2024-03-03 08:00:20 +0330

    edit: spellcheck

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from tuturial.quickstart import views
+from tutorial.quickstart import views
 
 
 
